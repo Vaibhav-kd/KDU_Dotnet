4aa1378358ec4ad57351962d413ba06d6d27afea
Priyanshi Agrawal <priyanshi2203@gmail.com>
2022-07-07 08:32:43 +0530

    xmlSerializer

---
+++ Session02/xmlSerialize/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System ;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+Person person =new Person(){FirstName="Priyanshi" , Lastname="Agrawal"};
+string path=".//link.xml";
+XMLSerializer xMLSerializer=new XMLSerializer();
+xMLSerializer.XmlSerializer(person,path);
+ 
+
+
+class XMLSerializer{
+public void XmlSerializer(object data, string path){
+    XmlSerializer xmlSerializer= new XmlSerializer(typeof(Person));
+    TextWriter writer = new StreamWriter(path);
+    xmlSerializer.Serialize(writer,data);
+    writer.Close();    
+}
+
+}
+
+public class Person{
+    public string FirstName{get; set;}
+    public string Lastname{get; set;}
+}
